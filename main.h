3a18c1807bf043424f8007c0dad1e7e189489d18
gerbias <smitnich@onid.orst.edu>
2015-12-14 19:37:40 -0800

    Finish switching to SDL_2

--- main.h
+++ main.h
@@ -1,8 +1,9 @@
 #include <string>
 
 struct SDL_Surface;
+struct SDL_Renderer;
 
 extern std::string rootPath;
 extern SDL_Surface *screen;
 extern SDL_Surface *getScreen();
-extern 	SDL_Renderer *renderer;
+extern SDL_Renderer *renderer;
